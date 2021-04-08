08cf711b72b440cf8eb6776c66768d9795fde1a9
ayselcavdar <a.misrikhanova13@gmail.com>
2021-04-08 22:34:42 +0300

    created footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="" width="100%">
+    <v-footer class="text-center" color="secondary" dark>
+      <v-card-text class="white--text">
+        <strong>© {{ new Date().getFullYear() }} Movie App :</strong
+        ><a
+          class="github ml-2"
+          href="https://www.linkedin.com/in/aysel-cavdar/"
+          target="_blank"
+          >Aysel Cavdar</a
+        >
+      </v-card-text>
+    </v-footer>
+  </v-card>
+</template>
+
+<style scoped>
+.github {
+  text-decoration: none;
+  color: #fff;
+}
+</style>
