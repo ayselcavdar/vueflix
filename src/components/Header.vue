cd3b1fd715623091c96b3f16d6c0c57dfba3685b
ayselcavdar <a.misrikhanova13@gmail.com>
2021-04-08 22:34:58 +0300

    created header component

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="mx-auto" width="100%">
+    <v-app-bar color="secondary" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>{{ titleText }}</v-toolbar-title>
+    </v-app-bar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["title"],
+  setup({ title }) {
+    return {
+      titleText: title,
+    };
+  },
+};
+</script>
